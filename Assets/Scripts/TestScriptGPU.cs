7ebbf3809887f1d6c6b8883b60d0db45d1c2d1d5
zhang <zhangowen213@utexas.edu>
2022-10-19 00:58:03 -0500

    Fixed compute shader drawing to make sure that objects closer to the camera are drawn in front of objects further away

--- Assets/Scripts/TestScriptGPU.cs
+++ Assets/Scripts/TestScriptGPU.cs
@@ -6,6 +6,7 @@ using K4AdotNet.Record;
 using K4AdotNet.Sensor;
 using TurboJpegWrapper;
 using System.Threading.Tasks;
+using UnityEngine.Profiling;
 
 public class TestScriptGPU : MonoBehaviour
 {
@@ -16,8 +17,10 @@ public class TestScriptGPU : MonoBehaviour
     Calibration kinectCalibration;
     Renderer rendererComponent;
     ComputeBuffer depthBuffer;
+    ComputeBuffer translationMapBuffer;
     static readonly int
         pixelBufferID = Shader.PropertyToID("pixelBuffer"),
+        translationMapBufferID = Shader.PropertyToID("translationMapBuffer"),
         outputBufferID = Shader.PropertyToID("outputBuffer"),
         depthBufferID = Shader.PropertyToID("depthBuffer"),
         imageWidthID = Shader.PropertyToID("imageWidth"),
@@ -26,45 +29,62 @@ public class TestScriptGPU : MonoBehaviour
     RenderTexture outputTexture;
     Texture2D tex;
     Texture2D blankBackground;
+    short[] blankDepth;
     short[] colorDepth;
     byte[] imgBuffer;
+    int[] translationMapBufferArr;
     TJDecompressor tJDecompressor;
     Image outputImg = new Image(ImageFormat.Depth16, 1280, 720, ImageFormats.StrideBytes(ImageFormat.Depth16, 1280));
+    int CSMainKernelID;
+    int DrawKernelID;
+    int imageWidth;
+    int imageHeight;
+    int[] mapArr;
     // Start is called before the first frame update
     void Start()
     {
+        CSMainKernelID = computeShader.FindKernel("CSMain");
+        DrawKernelID = computeShader.FindKernel("Draw");
         Physics.autoSimulation = false;
         kinectVideo = new Playback("C:/Users/zhang/OneDrive/Desktop/output.mkv");
         kinectVideo.GetCalibration(out kinectCalibration);
         kinectTransform = new Transformation(kinectCalibration);
+        imageWidth = kinectCalibration.ColorCameraCalibration.ResolutionWidth;
+        imageHeight = kinectCalibration.ColorCameraCalibration.ResolutionHeight;
 
         //kinectVideo.TrySeekTimestamp(K4AdotNet.Microseconds64.FromSeconds(5.8), PlaybackSeekOrigin.Begin);
         kinectVideo.TryGetNextCapture(out var capture);
         rendererComponent = GetComponent<Renderer>();
-        rt = new RenderTexture(1280, 720, 0);
+        rt = new RenderTexture(imageWidth, imageHeight, 0);
         rt.enableRandomWrite = true;
-        outputTexture = new RenderTexture(1280, 720, 0);
+        outputTexture = new RenderTexture(imageWidth, imageHeight, 0);
         outputTexture.enableRandomWrite = true;
         RenderTexture.active = outputTexture;
-        depthBuffer = new ComputeBuffer(1280 * 720 / 2, 4);
-        tex = new Texture2D(1280, 720, TextureFormat.RGBA32, false);
-        blankBackground = new Texture2D(1280, 720, TextureFormat.RGBA32, false);
-        for (int i = 0; i < 720; i++)
+        depthBuffer = new ComputeBuffer(imageWidth * imageHeight / 2, 4);
+        translationMapBuffer = new ComputeBuffer(imageWidth * imageHeight, 4);
+        tex = new Texture2D(imageWidth, imageHeight, TextureFormat.RGBA32, false);
+        blankBackground = new Texture2D(imageWidth, imageHeight, TextureFormat.RGBA32, false);
+        for (int i = 0; i < imageHeight; i++)
         {
-            for (int j = 0; j < 1280; j++)
+            for (int j = 0; j < imageWidth; j++)
             {
                 blankBackground.SetPixel(j, i, Color.black);
             }
         }
         blankBackground.Apply();
-        colorDepth = new short[1280 * 720];
-        imgBuffer = new byte[1280 * 720 * 4];
-        computeShader.SetInt(imageWidthID, 1280);
-        computeShader.SetInt(imageHeightID, 720);
-        computeShader.SetBuffer(0, depthBufferID, depthBuffer);
-        computeShader.SetTexture(0, pixelBufferID, rt);
-        computeShader.SetTexture(0, outputBufferID, outputTexture);
+        colorDepth = new short[imageWidth * imageHeight];
+        imgBuffer = new byte[imageWidth * imageHeight * 4];
+        translationMapBufferArr = new int[imageWidth * imageHeight];
+        computeShader.SetInt(imageWidthID, imageWidth);
+        computeShader.SetInt(imageHeightID, imageHeight);
+        computeShader.SetBuffer(CSMainKernelID, depthBufferID, depthBuffer);
+        computeShader.SetBuffer(CSMainKernelID, translationMapBufferID, translationMapBuffer);
+        computeShader.SetBuffer(DrawKernelID, translationMapBufferID, translationMapBuffer);
+        computeShader.SetTexture(DrawKernelID, pixelBufferID, rt);
+        computeShader.SetTexture(DrawKernelID, outputBufferID, outputTexture);
         tJDecompressor = new TJDecompressor();
+        mapArr = new int[imageHeight * imageWidth];
+        Application.targetFrameRate = 60;
     }
 
     private void OnEnable()
@@ -75,14 +95,11 @@ public class TestScriptGPU : MonoBehaviour
     private void OnDisable()
     {
         depthBuffer.Release();
+        translationMapBuffer.Release();
         kinectTransform.Dispose();
         kinectVideo.Dispose();
     }
 
-    void UpdateFunctionOnGPU()
-    {
-        computeShader.Dispatch(0, 1280 / 8, 720 / 8, 1);
-    }
     // Update is called once per frame
     void Update()
     {
@@ -106,20 +123,38 @@ public class TestScriptGPU : MonoBehaviour
                         //Run heavy CPU computations concurrently
                         var tasks = new[]
                         {
-                            Task.Run(() => kinectTransform.DepthImageToColorCamera(depthImg, outputImg)),
-                            Task.Run(() => tJDecompressor.Decompress(img.Buffer, (ulong)img.SizeBytes, imgBufferPtr, img.WidthPixels * img.HeightPixels * 4, TJPixelFormats.TJPF_RGBA, TJFlags.BOTTOMUP))
+                            Task.Run(() => DepthToColorTransformationProfiler(depthImg)),
+                            Task.Run(() => JPGDecompressProfiler(img, imgBufferPtr))
                         };
                         Task.WaitAll(tasks);
                     }
                 }
                 outputImg.CopyTo(colorDepth);
                 depthBuffer.SetData(colorDepth);
+                //compute mapping between pixels in image to where they will be translated
+                computeShader.Dispatch(CSMainKernelID, imageWidth / 8, imageHeight / 8, 1);
+                //waits for GPU to finish calculations, so ends up blocking for a significant amount of time. Maybe find out how to do the inverse mapping in the compute shader to avoid this.
+                translationMapBuffer.GetData(translationMapBufferArr);
+                //Find the inverse map from translated pixels to original pixels where any collision will prioritize pixels that are closer.
+                Profiler.BeginSample("Inverse map");
+                System.Array.Fill(mapArr, imageHeight * imageWidth);
+                List<Task> list = new List<Task>();
+                int numTasks = 8;
+                Task[] taskArr = new Task[numTasks];
+                for (int i = 0; i < numTasks; i++)
+                {
+                    int index = i;
+                    taskArr[i] = Task.Factory.StartNew(() => InverseMapProfiler(index, translationMapBufferArr.Length / numTasks));
+                }
+                Task.WaitAll(taskArr);
+                Profiler.EndSample();
+                translationMapBuffer.SetData(mapArr);
                 tex.LoadRawTextureData(imgBuffer);
                 tex.Apply();
                 Graphics.Blit(tex, rt);
                 Graphics.Blit(blankBackground, outputTexture);
-                //raw data is in the format of RGBA
-                UpdateFunctionOnGPU();
+                //draw pixels on screen
+                computeShader.Dispatch(DrawKernelID, imageWidth / 8, imageHeight / 8, 1);
                 rendererComponent.material.mainTexture = outputTexture;
             }
             if (img != null)
@@ -132,5 +167,44 @@ public class TestScriptGPU : MonoBehaviour
             }
             capture.Dispose();
         }
+    }
+
+    void DepthToColorTransformationProfiler(Image depthImg)
+    {
+        Profiler.BeginThreadProfiling("updateThreads", "DepthToColorTransformationProfiler Task " + Task.CurrentId);
+        kinectTransform.DepthImageToColorCamera(depthImg, outputImg);
+        Profiler.EndThreadProfiling();
+    }
+
+    void JPGDecompressProfiler(Image img, System.IntPtr imgBufferPtr)
+    {
+        Profiler.BeginThreadProfiling("updateThreads", "JPGDecompressProfiler Task " + Task.CurrentId);
+        tJDecompressor.Decompress(img.Buffer, (ulong)img.SizeBytes, imgBufferPtr, img.WidthPixels * img.HeightPixels * 4, TJPixelFormats.TJPF_RGBA, TJFlags.BOTTOMUP);
+        Profiler.EndThreadProfiling();
+    }
+
+    void InverseMapProfiler(int segment, int segmentSize)
+    {
+        Profiler.BeginThreadProfiling("updateThreads", "InverseMapProfiler Task " + Task.CurrentId);
+        for (int i = 0; i < segmentSize; i++)
+        {
+            int index = segment * segmentSize + i;
+            if (translationMapBufferArr[index] == imageHeight * imageWidth)
+            {
+                continue;
+            }
+            if (mapArr[translationMapBufferArr[index]] == imageHeight * imageWidth)
+            {
+                mapArr[translationMapBufferArr[index]] = index;
+            }
+            else
+            {
+                if (colorDepth[index] < colorDepth[mapArr[translationMapBufferArr[index]]])
+                {
+                    mapArr[translationMapBufferArr[index]] = index;
+                }
+            }
+        }
+        Profiler.EndThreadProfiling();
     }
 }
